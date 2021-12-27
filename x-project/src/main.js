ba7564b9e11412faae9347ff4258e3ac5fc04257
Unknown <1541738701@qq.com>
2021-12-27 20:59:55 +0800

    添加axios交互代码

--- x-project/src/main.js
+++ x-project/src/main.js
@@ -7,7 +7,10 @@ import 'element-ui/lib/theme-chalk/index.css'; // 导入相关样式
 import './assets/js/dialog-drag.js'
 import './assets/js/rem.js'
 import './assets/sass/element-reset.scss' //element样式修改
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL = 'http://localhost:80'
+Vue.use(VueAxios,axios);
 Vue.use(ElementUI); // 引用
 Vue.config.productionTip = false
 
