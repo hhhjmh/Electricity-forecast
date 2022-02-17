a36680c12e1b702809c1b9b1405c87f719f80dfe
Chikage0o0 <chikage@939.me>
2022-02-17 16:02:48 +0800

    添加GET请求支持

--- power_prediction/src/main/java/com/example/power_prediction/controller/PowerDistributionHourController.java
+++ power_prediction/src/main/java/com/example/power_prediction/controller/PowerDistributionHourController.java
@@ -14,17 +14,17 @@ public class PowerDistributionHourController {
     @Autowired
     PowerDistributionHourService powerDistributionHourService;
 
-    @PostMapping("/queryDay")
+    @RequestMapping("/queryDay")
     public Map<String, Object> queryDay(@RequestParam Integer deviceId, @RequestParam Integer year, @RequestParam Integer month, @RequestParam Integer day) {
         return powerDistributionHourService.queryByDay(deviceId, year, month, day);
     }
 
-    @PostMapping("/hourlyUpdate")
+    @RequestMapping("/hourlyUpdate")
     public Map<String, Object> hourlyUpdate() {
         return powerDistributionHourService.hourlyUpdate();
     }
 
-    @PostMapping("/queryCustom")
+    @RequestMapping("/queryCustom")
     public Map<String, Object> queryCustom(@RequestParam String deviceIds, @RequestParam Integer start, @RequestParam Integer end, @RequestParam Integer unit) {
         return powerDistributionHourService.queryByDayMulti(deviceIds, start, end, unit);
     }
