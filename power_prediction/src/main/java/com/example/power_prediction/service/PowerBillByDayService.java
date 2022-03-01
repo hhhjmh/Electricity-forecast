9d3973f3782caebab13b1379b51a4cdaadc4a7ab
Chikage0o0 <chikage@939.me>
2022-03-01 16:50:06 +0800

    修改注释

--- power_prediction/src/main/java/com/example/power_prediction/service/PowerBillByDayService.java
+++ power_prediction/src/main/java/com/example/power_prediction/service/PowerBillByDayService.java
@@ -11,9 +11,9 @@ public interface PowerBillByDayService {
 
     /**
      * @param deviceIds 设备id，多个用","分割
-     * @param start     开始时间，单位为day 则为2022-01-01 单位为month则为 2022-01
-     * @param end       结束时间，单位为day 则为2022-01-01 单位为month则为 2022-01
-     * @param unit      单位 day或者month
+     * @param start     开始时间，单位为day 则为2022-01-01 单位为month则为 2022-01 单位为year则为 2022
+     * @param end       结束时间，单位为day 则为2022-01-01 单位为month则为 2022-01 单位为year则为 2022
+     * @param unit      单位 day、month、year
      * @return 第一层 时间 第二次 设备id 第三层数据
      */
     Map<String, Object> queryCustom(String deviceIds, String start, String end, String unit);
