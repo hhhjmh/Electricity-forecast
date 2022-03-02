e707f8f1cf693227a8cbd96fd5f90f1cc782a093
hhhjmh <1418964647@qq.com>
2022-03-02 09:51:14 +0800

    耗电时比分析1

--- power_prediction/src/main/java/com/example/power_prediction/repository/PowerAnalyseMonthAvgRepository.java
+++ power_prediction/src/main/java/com/example/power_prediction/repository/PowerAnalyseMonthAvgRepository.java
@@ -3,6 +3,10 @@ package com.example.power_prediction.repository;
 import com.example.power_prediction.entity.PowerAnalyseMonthAvg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PowerAnalyseMonthAvgRepository extends JpaRepository<PowerAnalyseMonthAvg,Integer> {
     PowerAnalyseMonthAvg findByDeviceIdAndDataTime(int deviceId, Integer dataTime);
+
+    List<PowerAnalyseMonthAvg> findAllByDeviceIdAndDataTimeBetween(Integer deviceId, Integer start, Integer end);
 }
--- power_prediction/src/main/java/com/example/power_prediction/repository/PowerAnalyseMonthMaxRepository.java
+++ power_prediction/src/main/java/com/example/power_prediction/repository/PowerAnalyseMonthMaxRepository.java
@@ -3,6 +3,10 @@ package com.example.power_prediction.repository;
 import com.example.power_prediction.entity.PowerAnalyseMonthMax;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PowerAnalyseMonthMaxRepository extends JpaRepository<PowerAnalyseMonthMax,Integer> {
     PowerAnalyseMonthMax findByDeviceIdAndDataTime(int deviceId, Integer dataTime);
+
+    List<PowerAnalyseMonthMax> findAllByDeviceIdAndDataTimeBetween(Integer deviceId, Integer start, Integer end);
 }
